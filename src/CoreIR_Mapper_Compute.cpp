ad834abc7f9fc23b0616df864886d07b474b38e7
Dillon Huff <dillonhuff@gmail.com>
2020-01-17 11:14:36 -0800

    Returning an actual compute unit from compute unit generation

--- src/CoreIR_Mapper_Compute.cpp
+++ src/CoreIR_Mapper_Compute.cpp
@@ -96,15 +96,16 @@ namespace Halide {
           return Evaluate::make(0);
         }
     };
-    
-    void generate_mapped_buffers(Stmt& s,
-        std::map<std::string, Function>& env,
-        std::vector<HWXcel>& buf_xcels) {
-      synthesize_hwbuffers(s, env, buf_xcels);
-      generate_compute_unit(s, env);
-    }
 
-    void generate_compute_unit(Stmt& stmt, std::map<std::string, Function>& env) {
+    class ComputeUnit {
+      public:
+        CoreIR::Module* mod;
+        std::map<const Variable*, CoreIR::Wireable*> vars;
+        std::map<const Call*, CoreIR::Wireable*> inputs;
+        std::map<const Provide*, CoreIR::Wireable*> outputs;
+    };
+    
+    ComputeUnit generate_compute_unit(Stmt& stmt, std::map<std::string, Function>& env) {
       auto pre_simple = simplify(stmt);
       cout << "Pre simplification..." << endl;
       cout << pre_simple << endl;
@@ -112,89 +113,102 @@ namespace Halide {
       cout << "Simplified code for stmt:" << endl;
       cout << simple << endl;
 
-      {
-        CoreIR::Context* context = newContext();
-        loadHalideLib(context);
-        CoreIRLoadLibrary_commonlib(context);
-        auto ns = context->getNamespace("global");
+      CoreIR::Context* context = newContext();
+      loadHalideLib(context);
+      CoreIRLoadLibrary_commonlib(context);
+      auto ns = context->getNamespace("global");
 
-        string hw_name = "";
-        for (auto f : env) {
-          cout << "Checking f: " << f.first << " to see if it is accelerator output" << endl;
-          if (f.second.schedule().is_accelerator_output()) {
-            hw_name = f.first;
-            break;
-          }
+      string hw_name = "";
+      for (auto f : env) {
+        cout << "Checking f: " << f.first << " to see if it is accelerator output" << endl;
+        if (f.second.schedule().is_accelerator_output()) {
+          hw_name = f.first;
+          break;
         }
-        if (hw_name != "") {
-          internal_assert(hw_name != "") << "No function to accelerate\n";
+      }
+      if (hw_name != "") {
+        internal_assert(hw_name != "") << "No function to accelerate\n";
 
-          RecordType* mtp = context->Record({{"clk", context->Named("coreir.clkIn")}, {"rst", context->BitIn()}});
-          auto m = ns->newModuleDecl("m", mtp);
-          auto mDef = m->newModuleDef();
+        RecordType* mtp = context->Record({{"clk", context->Named("coreir.clkIn")}, {"rst", context->BitIn()}});
+        auto m = ns->newModuleDecl("m", mtp);
+        auto mDef = m->newModuleDef();
 
-          ProduceFinder rFinder(hw_name);
-          simple->accept(&rFinder);
-          internal_assert(rFinder.r != nullptr);
+        ProduceFinder rFinder(hw_name);
+        simple->accept(&rFinder);
+        internal_assert(rFinder.r != nullptr);
 
-          vector<pair<string, CoreIR::Type*> > compute_fields{{"clk", context->Named("coreir.clkIn")}, {"rst", context->BitIn()}};
-          ComputeExtractor ce;
-          Stmt compute_only = simplify(ce.mutate(rFinder.r->body));
-          cout << "Compute logic..." << endl;
-          cout << compute_only << endl;
-          HardwareInfo info;
-          info.interfacePolicy =
-            HW_INTERFACE_POLICY_COMPUTE_UNIT;
+        vector<pair<string, CoreIR::Type*> > compute_fields{{"clk", context->Named("coreir.clkIn")}, {"rst", context->BitIn()}};
+        ComputeExtractor ce;
+        Stmt compute_only = simplify(ce.mutate(rFinder.r->body));
+        cout << "Compute logic..." << endl;
+        cout << compute_only << endl;
+        HardwareInfo info;
+        info.interfacePolicy =
+          HW_INTERFACE_POLICY_COMPUTE_UNIT;
 
-          //Expr inLen((int) ce.callNums.size());
-          Expr inLen((int) ce.ld_inst);
-          Range rng(Expr((int) 0), inLen);
-          Range z(Expr((int) 0), Expr((int) 1));
-          vector<CoreIR_Argument> compute_args;
-          Stencil_Type inTp{Stencil_Type::StencilContainerType::AxiStream,
-            Int(16),
-            {z, rng},
-            0};
-          compute_args.push_back({"compute_input.stencil", true, false, Int(16), inTp});
+        //Expr inLen((int) ce.callNums.size());
+        Expr inLen((int) ce.ld_inst);
+        Range rng(Expr((int) 0), inLen);
+        Range z(Expr((int) 0), Expr((int) 1));
+        vector<CoreIR_Argument> compute_args;
+        Stencil_Type inTp{Stencil_Type::StencilContainerType::AxiStream,
+          Int(16),
+          {z, rng},
+          0};
+        compute_args.push_back({"compute_input.stencil", true, false, Int(16), inTp});
 
-          //Expr outLen((int) ce.provideNums.size());
-          Expr outLen((int) ce.st_inst);
-          cout << "compute_result outLen = " << outLen << endl;
-          cout << "---- Provides" << endl;
-          for (auto pr : ce.provideNums) {
-            cout << "\t" << pr.second << endl;
-          }
-          cout << "compute st nums       = " << ce.st_inst << endl;
-          Range outRng(Expr((int) 0), outLen);
-          Stencil_Type outTp{Stencil_Type::StencilContainerType::AxiStream,
-            Int(16),
-            {z, outRng},
-            0};
-          compute_args.push_back({"compute_result.stencil", true, true, Int(16), outTp});
-          Closure interface;
-          compute_only.accept(&interface);
-          cout << "Interface..." << endl;
-          cout << "\tExternal vars..." << endl;
-          for (auto v : interface.vars) {
-            cout << "\t\t" << v.first << endl;
-            compute_args.push_back({v.first, false, false, v.second, outTp});
-          }
-
-          //cout << "\t# external buffers = " << interface.buffers.size() << endl;
-          //internal_assert(interface.buffers.size() == 0);
-          auto compute_unit =
-            createCoreIRForStmt(context, info, compute_only, "compute_unit", compute_args);
-          mDef->addInstance("compute_unit", compute_unit);
-
-
-          m->setDef(mDef);
-          cout << "Output module" << endl;
-          m->print();
+        //Expr outLen((int) ce.provideNums.size());
+        Expr outLen((int) ce.st_inst);
+        cout << "compute_result outLen = " << outLen << endl;
+        cout << "---- Provides" << endl;
+        for (auto pr : ce.provideNums) {
+          cout << "\t" << pr.second << endl;
+        }
+        cout << "compute st nums       = " << ce.st_inst << endl;
+        Range outRng(Expr((int) 0), outLen);
+        Stencil_Type outTp{Stencil_Type::StencilContainerType::AxiStream,
+          Int(16),
+          {z, outRng},
+          0};
+        compute_args.push_back({"compute_result.stencil", true, true, Int(16), outTp});
+        Closure interface;
+        compute_only.accept(&interface);
+        cout << "Interface..." << endl;
+        cout << "\tExternal vars..." << endl;
+        for (auto v : interface.vars) {
+          cout << "\t\t" << v.first << endl;
+          compute_args.push_back({v.first, false, false, v.second, outTp});
         }
 
-        deleteContext(context);
+        //cout << "\t# external buffers = " << interface.buffers.size() << endl;
+        //internal_assert(interface.buffers.size() == 0);
+        auto compute_unit =
+          createCoreIRForStmt(context, info, compute_only, "compute_unit", compute_args);
+        mDef->addInstance("compute_unit", compute_unit);
+
+
+        m->setDef(mDef);
+        cout << "Output module" << endl;
+        m->print();
+        return {m};
+      }
+
+
+      //deleteContext(context);
+      return {nullptr};
+    }
+
+    void generate_mapped_buffers(Stmt& s,
+        std::map<std::string, Function>& env,
+        std::vector<HWXcel>& buf_xcels) {
+      synthesize_hwbuffers(s, env, buf_xcels);
+      ComputeUnit cu = generate_compute_unit(s, env);
+      if (cu.mod != nullptr) {
+        cout << "Compute unit..." << endl;
+        cu.mod->print();
       }
     }
+
 
 
   }
--- src/CoreIR_Mapper_Compute.h
+++ src/CoreIR_Mapper_Compute.h
@@ -8,7 +8,6 @@
 namespace Halide {
   namespace Internal {
 
-    void generate_compute_unit(Stmt& stmt, std::map<std::string, Function>& env);
     void generate_mapped_buffers(Stmt& stmt,
         std::map<std::string, Function>& env,
         std::vector<HWXcel>& xcels);
