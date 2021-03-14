70190fdab8a9c15e2af0a7348d905ed4202a04f5
Sho Ko <kosho2013@hotmail.com>
2021-03-14 15:42:33 -0700

    audio_pipeline

--- apps/hardware_benchmarks/apps/audio_pipeline/audio_pipeline_generator.cpp
+++ apps/hardware_benchmarks/apps/audio_pipeline/audio_pipeline_generator.cpp
@@ -1,13 +1,4 @@
-/*
- * An application that performs a basic guitar pedal pipeline.
- * Stages of the pipeline are compression, distortion, noise
- * reduction, modulation, and reverberation.
- */
-
 #include "Halide.h"
-
-const float PI = 3.1415926536f;
-
 
 namespace {
   using namespace Halide;
@@ -17,24 +8,27 @@ namespace {
   class AudioPipeline : public Halide::Generator<AudioPipeline> {
     
   public:
-    Input<Buffer<int16_t>>  input{"input", 2}; // second dimension is each audio channel
+    Input<Buffer<int16_t>>  in1{"in1", 2};
+    Input<Buffer<int16_t>>  in2{"in2", 2};
     Output<Buffer<int16_t>> output{"output", 2};
 
     void generate() {
-	  float sample_freq = 44100;
-	  int num_samples = 31466;
-
+		
+	  float sample_freq = 48000;
+	  int num_samples = 868346;
+	  float PI = atan(1)*4;
 
 
 	  Var t, c;
 
-      Func hw_input, hw_output;
+      Func hw_input1, hw_input2, hw_output;
 	  Func normalize, scale_up;
 	  
-      hw_input(t, c) = input(t, c);
+      hw_input1(t, c) = in1(t, c);
+      hw_input2(t, c) = in2(t, c);
 	  
-	  normalize(t, c) = cast<float>(hw_input(t, c)) / 32767.0f;
-	  scale_up(t, c) = cast<int16_t>(clamp(10.0f * normalize(t, c), -1.0f, 1.0f) * 32767.0f);
+	  normalize(t, c) = cast<float>(hw_input1(t, c)) / 32767.0f + cast<float>(hw_input2(t, c)) / 32767.0f;
+	  scale_up(t, c) = cast<int16_t>(clamp(3.0f * normalize(t, c), -1.0f, 1.0f) * 32767.0f);
 
 	  hw_output(t, c) = scale_up(t, c);
 	  output(t, c) = hw_output(t, c);
@@ -45,7 +39,7 @@ namespace {
 		Var ti, to;  
 		  
         output.bound(t, 0, num_samples);
-        output.bound(c, 0, 2);
+        output.bound(c, 0, 1);
 
         hw_output.compute_root();
 
@@ -58,7 +52,8 @@ namespace {
         normalize.compute_at(hw_output, to);
 
 
-        hw_input.stream_to_accelerator();
+        hw_input2.stream_to_accelerator();
+        hw_input1.stream_to_accelerator();
           
       } else 
 	  {    
--- apps/hardware_benchmarks/apps/audio_pipeline/process.cpp
+++ apps/hardware_benchmarks/apps/audio_pipeline/process.cpp
@@ -19,12 +19,11 @@ using namespace std;
 
 int main( int argc, char **argv ) {
   std::map<std::string, std::function<void()>> functions;
-  OneInOneOut_ProcessController<int16_t> processor("audio_pipeline");
-  //OneInOneOut_ProcessController<int16_t> processor("audio_pipeline");
+  ManyInOneOut_ProcessController<int16_t> processor("audio_pipeline", {"in1.png", "in2.png"});
 
   #if defined(WITH_CPU)
       auto cpu_process = [&]( auto &proc ) {
-        audio_pipeline( proc.input, proc.output );
+        audio_pipeline( proc.inputs["in1.png"], proc.inputs["in2.png"], proc.output );
       };
       functions["cpu"] = [&](){ cpu_process( processor ); } ;
   #endif
@@ -34,7 +33,7 @@ int main( int argc, char **argv ) {
         RDAI_Platform *rdai_platform = RDAI_register_platform( &rdai_clockwork_sim_ops );
         if ( rdai_platform ) {
           printf( "[RUN_INFO] found an RDAI platform\n" );
-          audio_pipeline_clockwork( proc.input, proc.output );
+          audio_pipeline( proc.inputs["in1.png"], proc.inputs["in2.png"], proc.output );
           RDAI_unregister_platform( rdai_platform );
         } else {
           printf("[RUN_INFO] failed to register RDAI platform!\n");
@@ -43,28 +42,32 @@ int main( int argc, char **argv ) {
       functions["clockwork"] = [&](){ clockwork_process( processor ); };
   #endif
 
-  // Add all defined functions
+
   processor.run_calls = functions;
   
   
   AudioFile<int16_t> f1;
-  readAudioData<int16_t>("third.wav", &f1);
+  readAudioData<int16_t>("radio_test.wav", &f1);
   
-  cout << "*********************************************************" << endl;
-  cout << f1.header << endl;
-  
+  AudioFile<int16_t> f2;
+  readAudioData<int16_t>("lecture_test.wav", &f2);
   
 
-  processor.input   = f1.data;
-  auto in = processor.input;
-  processor.output  = Buffer<int16_t>(in.dim(0).extent(), in.dim(1).extent());
-  processor.inputs_preset = true;
+  processor.inputs["in1.png"]   = f1.data;
+  processor.inputs["in2.png"]   = f2.data;
+  
+  auto in1 = processor.inputs["in1.png"];
+  auto in2 = processor.inputs["in2.png"];
   
   
-  cout << in.dim(0).extent() << "   " << in.dim(1).extent() << endl;
+  cout << "*********************************************************" << endl;
+  cout << "radio_test: " << in1.dim(0).extent() << "   " << in1.dim(1).extent() << endl;
+  cout << "lecture_test: " << in2.dim(0).extent() << "   " << in2.dim(1).extent() << endl;
   cout << "*********************************************************" << endl;
   
   
+  processor.output  = Buffer<int16_t>(in1.dim(0).extent(), in1.dim(1).extent());
+  processor.inputs_preset = true;
 
   auto ret_value = processor.process_command(argc, argv);
 
@@ -75,20 +78,6 @@ int main( int argc, char **argv ) {
   writeAudioData<int16_t>("bin/output.wav", outAudio);
   
   auto out = processor.output;
-  
-  
-  
-  // for (int i = 0; i < 40; i++)
-  // {
-	  // cout << in(i, 0) << "  " << in(i, 1) << endl;
-  // }
-  
-  // cout << endl << endl << endl;
-  
-  // for (int i = 0; i < 40; i++)
-  // {
-	  // cout << out(i, 0) << "  " << out(i, 1) << endl;
-  // }
-  
+
   return ret_value;
 }
